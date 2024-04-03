a2342b258ca4ee88b1cfac4f17aa9406ed58794b
danielecolaci <danielecolaci@outlook.com>
2024-04-03 12:20:00 +0200

    Install Axios and set key api url

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,17 @@
-<script></script>
+<script>
+import { state } from './state.js'
+
+export default {
+  data() {
+    return {
+      state
+    }
+  },
+  mounted() {
+    this.state.callApi(this.state.key_api_url)
+  }
+}
+</script>
 
 <template></template>
 
---
+++ src/state.js
@@ -0,0 +1,14 @@
+import { reactive } from 'vue';
+import axios from 'axios';
+
+export const state = reactive({
+    key_api_url: 'https://api.themoviedb.org/3/search/movie?api_key=d0d52716cddf9984f802b42762f6e17f',
+
+    callApi(url) {
+        axios
+            .get(url)
+            .then(response => {
+                console.log(response);
+            })
+    }
+})
