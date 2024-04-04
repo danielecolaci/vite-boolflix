82791155240e230df75f66480d2be1bc22c6d85c
danielecolaci <danielecolaci@outlook.com>
2024-04-04 18:24:34 +0200

    Add Star Rating function

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -7,6 +7,13 @@ export default {
             const initalUrl = 'https://image.tmdb.org/t/p/';
             const size = 'w342';
             return `${initalUrl}${size}/${image}`
+        },
+        starNumber(star, rating) {
+            if (star <= Math.round(rating / 2)) {
+                return 'fa-solid';
+            } else {
+                return 'fa-regular';
+            }
         }
     }
 }
@@ -23,7 +30,9 @@ export default {
                 <h5>{{ movie.title }}</h5>
                 <h6 v-if="movie.title !== movie.original_title">{{ movie.original_title }}</h6>
                 <p>{{ movie.original_language }}</p>
-                <p>{{ movie.vote_average }}</p>
+                <div>
+                    <i v-for="star in 5" :key="star" :class="[starNumber(star, movie.vote_average), 'fa-star']"></i>
+                </div>
             </div>
         </div>
         <p v-else>No movies found</p>
@@ -37,7 +46,9 @@ export default {
                 <h5>{{ serie.name }}</h5>
                 <h6 v-if="serie.name !== serie.original_name">{{ serie.original_name }}</h6>
                 <p>{{ serie.original_language }}</p>
-                <p>{{ serie.vote_average }}</p>
+                <div>
+                    <i v-for="star in 5" :key="star" :class="[starNumber(star, serie.vote_average), 'fa-star']"></i>
+                </div>
             </div>
         </div>
         <p v-else>No series found</p>
