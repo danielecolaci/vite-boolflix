9acdbc209edc59f43238f2d3e1d4867a53a7c23c
danielecolaci <danielecolaci@outlook.com>
2024-04-04 17:22:48 +0200

    Add image url

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,32 +1,45 @@
 <script>
 export default {
     name: 'AppMain',
-    props: ['movies', 'series']
+    props: ['movies', 'series'],
+    methods: {
+        imageUrl(image) {
+            const initalUrl = 'https://image.tmdb.org/t/p/';
+            const size = 'w342';
+            return `${initalUrl}${size}/${image}`
+        }
+    }
 }
 </script>
 
 <template>
     <div>
-        <h3>Results Movies</h3>
-        <ul v-if="movies.length > 0">
-            <li v-for="movie in movies" :key="movie.id">
+        <h2>Results Movies</h2>
+        <div class="content" v-if="movies.length > 0" v-for="movie in movies" :key="movie.id">
+            <div class="image">
+                <img :src="imageUrl(movie.poster_path)" alt="">
+            </div>
+            <div class="info">
                 <h5>{{ movie.title }}</h5>
                 <h6 v-if="movie.title !== movie.original_title">{{ movie.original_title }}</h6>
                 <p>{{ movie.original_language }}</p>
                 <p>{{ movie.vote_average }}</p>
-            </li>
-        </ul>
+            </div>
+        </div>
         <p v-else>No movies found</p>
 
-        <h3>Results Tv Series</h3>
-        <ul v-if="series.length > 0">
-            <li v-for="serie in series" :key="serie.id">
+        <h2>Results Tv Series</h2>
+        <div class="content" v-if="series.length > 0" v-for="serie in series" :key="serie.id">
+            <div class="image">
+                <img :src="imageUrl(serie.poster_path)" alt="">
+            </div>
+            <div class="info">
                 <h5>{{ serie.name }}</h5>
-                <h6 v-if="serie.title !== serie.original_title">{{ serie.original_name }}</h6>
+                <h6 v-if="serie.name !== serie.original_name">{{ serie.original_name }}</h6>
                 <p>{{ serie.original_language }}</p>
                 <p>{{ serie.vote_average }}</p>
-            </li>
-        </ul>
+            </div>
+        </div>
         <p v-else>No series found</p>
     </div>
 </template>
